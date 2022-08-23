fdc875b89930713ccfb834e70437ba0ab50e59db
BanYi <Ban@atbanyi.com>
2022-08-23 12:46:58 +0800

    second commit

--- src/main/java/com/atbanyi/git/GitTest.java
+++ src/main/java/com/atbanyi/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atbanyi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello dit!");
+        System.out.println("hello dit2!");
     }
 }
